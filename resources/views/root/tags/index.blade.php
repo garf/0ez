cc7e22ea94a34d723639f6b16e94659225374617
Dinar Garipov <garipov.dinar@gmail.com>
2015-07-23 10:49:45 +0300

    users page in work

--- resources/views/root/tags/index.blade.php
+++ resources/views/root/tags/index.blade.php
@@ -7,20 +7,18 @@
         <div class="well well-sm text-right">
             <a href="{{ route('root-tags-clear-orphaned') }}" onclick="return confirm('Are you sure?');" class="btn btn-info">Clean Orphaned Tags</a>
         </div>
-        <div class="row">
-            <div class="col-sm-12 col-md-12 col-lg-12">
-                <ul class="list-group">
-                    @foreach($tags as $tag)
-                        <li class="list-group-item active">
-                            <span class="badge" title="Posts count">{{ $tag->num }}</span>
-                            <a class="collection-item brown-text" href="{{ route('tag', ['tag' => $tag->tag]) }}" target="_blank">
-                                {{ $tag->tag }}&nbsp;
-                            </a>
-                            <a href="{{ route('root-tags-remove', ['tag_id' => $tag->id]) }}" onclick="return confirm('You want to remove this tag?');"><i class="fa fa-times"></i></a>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
+        <div class="">
+            <ul class="list-group">
+                @foreach($tags as $tag)
+                    <li class="list-group-item active">
+                        <span class="badge" title="Posts count">{{ $tag->num }}</span>
+                        <a class="collection-item brown-text" href="{{ route('tag', ['tag' => $tag->tag]) }}" target="_blank">
+                            {{ $tag->tag }}&nbsp;
+                        </a>
+                        <a href="{{ route('root-tags-remove', ['tag_id' => $tag->id]) }}" onclick="return confirm('You want to remove this tag?');"><i class="fa fa-times"></i></a>
+                    </li>
+                @endforeach
+            </ul>
         </div>
     </div>
 @stop
--- resources/views/root/users/index.blade.php
+++ resources/views/root/users/index.blade.php
@@ -4,7 +4,20 @@
     <div class="container">
         <h1>{{ $title }}</h1>
         <div>
-
+            @foreach($users as $user)
+                <div class="row">
+                    <div class="col-lg-1">{{ $user->id }}</div>
+                    <div class="col-lg-4">
+                        <div>{{ $user->name }}</div>
+                        <div class="text-muted">{{ $user->email }}</div>
+                    </div>
+                    <div class="col-lg-4">{{ $user->created_at }}</div>
+                    <div class="col-lg-3 text-right">
+                        {{--<a href="#" class="btn btn-primary"><i class="fa fa-pencil"></i></a>--}}
+                    </div>
+                </div>
+                <hr />
+            @endforeach
         </div>
     </div>
 @stop
