6c44d2aa1e621ccabc64b812defb37da340af56e
Dinar Garipov <garipov.dinar@gmail.com>
2015-07-29 14:47:06 +0300

    appearance

--- resources/views/site/posts/view.blade.php
+++ resources/views/site/posts/view.blade.php
@@ -3,46 +3,44 @@
 @section('body')
     <div class="container">
         <div class="row">
-            <div class="col-sm-12 col-md-12 col-lg-9 post-body">
-                <article>
-                    <div class="card">
-                        <div class="card-content">
-                            <h1 class="card-title blue-grey-text darken-4">{{ $post->title }}</h1>
-
-                            <div class="row">
-                                <div class="col-sm-12 col-md-12 col-lg-12">
-                                    <div class="card-image">
-                                        <img src="/upload/{{ $post->img }}" alt="" style="max-width: 100%;" class="circle responsive-img">
+            <div class="col-sm-12 col-md-12 col-lg-9">
+                <div class="post-body">
+                    <img src="/upload/{{ $post->img }}" alt="" style="max-width: 100%;" class="circle responsive-img">
+                    <article>
+                        <div class=" post-content">
+                            <div class="">
+                                <h1 class="">{{ $post->title }}</h1>
+                                <div class="row">
+                                    <div class="col-sm-12 col-md-12 col-lg-12">
+                                        <div class="">
+                                            {!! $post->content !!}
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-12 col-md-12 col-lg-12">
-                                    <br />
-                                    {!! $post->content !!}
-                                </div>
-                            </div>
-                            <hr />
-                            <div>
+                                <hr />
                                 <div>
-                                    Категория:
-                                    <a href="{{ route('category', ['slug' => $post->category->slug]) }}">{{ $post->category->title }}</a>
+                                    <div>
+                                        Категория:
+                                        <a href="{{ route('category', ['slug' => $post->category->slug]) }}">{{ $post->category->title }}</a>
+                                    </div>
+                                    <div>
+                                        Автор: {{ $post->user->name }}
+                                    </div>
+                                    <div>
+                                        Опубликовано: {{ hdate($post->published_at) }}
+                                    </div>
+                                    <div>
+                                        Теги: @include('site.partials.tags-list', ['tags' => $post->tags])
+                                    </div>
                                 </div>
-                                <div>
-                                    Автор: {{ $post->user->name }}
-                                </div>
-                                <div>
-                                    Опубликовано: {{ hdate($post->published_at) }}
-                                </div>
-                                <div>
-                                    Теги: @include('site.partials.tags-list', ['tags' => $post->tags])
-                                </div>
-                            </div>
 
+                            </div>
+                            <div class="">
+                                @include('site.partials.related', ['post' => $post])
+                            </div>
                         </div>
-                        <div class="">
-                            @include('site.partials.related', ['post' => $post])
-                        </div>
-                    </div>
-                </article>
+                    </article>
+                </div>
             </div>
             <div class="col-sm-12 col-md-12 col-lg-3">
                 @include('site.partials.categories-menu')
