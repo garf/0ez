7e4b981b35e71b7091c8ea4005fb8ee68c7e97ee
Dinar Garipov <garipov.dinar@gmail.com>
2015-08-19 13:23:25 -0400

    fixed social links

--- resources/views/site/partials/social-links.blade.php
+++ resources/views/site/partials/social-links.blade.php
@@ -3,13 +3,13 @@
         @if(Conf::get('social.links.' . $service['name'], '') != '')
             <div class="col-md-3 col-sm-6 col-xs-12 text-center" style="margin-bottom: 30px;">
                 <div>
-                    <a href="{{ Conf::get('social.' . $service['name'] . '.link') }}" target="_blank">
+                    <a href="{{ Conf::get('social.links.' . $service['name']) }}" target="_blank">
                         <i class="fa {{ $service['icon'] }} fa-2x" style="color: {{ $service['color'] }};"></i>
                     </a>
                 </div>
                 @if(Conf::get('social.show_titles', false))
                     <div>
-                        <a href="{{ Conf::get('social.' . $service['name'] . '.link') }}" target="_blank">
+                        <a href="{{ Conf::get('social.links.' . $service['name']) }}" target="_blank">
                             {{ $service['title'] }}
                         </a>
                     </div>
