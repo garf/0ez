e078b73205c8b23bb32ba9c999e9a1dccc0b69d7
Dinar Garipov <garipov.dinar@gmail.com>
2015-11-12 15:35:40 +0300

    Fixed social links old system exception in frontend

--- app/Http/ViewComposers/SocialLinksComposer.php
+++ app/Http/ViewComposers/SocialLinksComposer.php
@@ -10,7 +10,12 @@ class SocialLinksComposer
 
     public function compose(View $view)
     {
-        $view->with('services', Conf::get('social.links'));
+        $links = Conf::get('social.links');
+        if (!isset($links[0]['url'])) {
+            $links = [];
+            Conf::set('social.links', $links);
+        }
+        $view->with('services', $links);
     }
 
 }
