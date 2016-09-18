92061cc66112ccf0b8e627c05a24cc8222db3fb2
Dinar Garipov <garf@users.noreply.github.com>
2016-09-18 16:06:17 +0200

    Fixed search button

--- resources/views/site/partials/search-form.blade.php
+++ resources/views/site/partials/search-form.blade.php
@@ -2,7 +2,7 @@
     <div class="input-group">
         <input type="text" name="q" class="form-control" placeholder="Поиск..." value="{{ $q }}">
       <span class="input-group-btn">
-        <button class="btn btn-primary" type="button">Искать</button>
+        <button class="btn btn-primary" type="submit">Искать</button>
       </span>
     </div>
 </form>
